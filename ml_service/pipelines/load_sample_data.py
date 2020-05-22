b7e26f8e52f953425281965d73814108810575b2
benjaminbluhm <benjaminbluhm@gmx.de>
2020-05-22 19:57:04 +0200

    Updated sample data creation

--- ml_service/pipelines/load_sample_data.py
+++ ml_service/pipelines/load_sample_data.py
@@ -4,5 +4,6 @@ import pandas as pd
 # Loads the sample data and produces a csv file that can
 # be used by the build/train pipeline script.
 def create_sample_data_csv():
-    df = pd.read_csv('D:/VSCodeProjects/AzureRecommender/data/aml_recommender.csv')
+    df = pd.read_csv('D:/VSCodeProjects/AzureRecommender/'
+                     'data/aml_recommender.csv')
     df.to_csv('aml_recommender.csv', index=False)
